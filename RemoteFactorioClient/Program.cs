6cea2c08b8b611d0a1f60d27151cbe9cf3806463
oxypomme <oxytom8@gmail.com>
2019-10-30 17:18:32 +0100

    updated libs

--- RemoteFactorioClient/Program.cs
+++ RemoteFactorioClient/Program.cs
@@ -4,7 +4,7 @@
 
 using System.IO;
 
-
+using Newtonsoft.Json;
 
 namespace RemoteFactorioServer
 {
--- RemoteFactorioServer/Server.cs
+++ RemoteFactorioServer/Server.cs
@@ -3,7 +3,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.IO;
-using System.Collections.Generic;
 using System.Diagnostics;
 using Newtonsoft.Json;
 
